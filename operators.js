9eace3d6a948be8675b349e238bf376f16a2cdd2
bastolaroman1-pixel <bastolaroman1@gmail.com>
2026-06-08 21:07:02 +0545

    update version

---
+++ operators.js
@@ -0,0 +1,11 @@
+// different operators
+
+let x=3
+let y=2
+
+console.log("Addition",x+y)
+console.log("Subtraction",x-y)
+console.log("Multiplication",x*y)
+console.log("Division",x/y)
+console.log("Remainder",x%y)
+console.log("Exponent",x**y)
